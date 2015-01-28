5508ef2aa86d6a21e875854bbd4aa763dcad53aa
1000ch <shogo.sensui@gmail.com>
2015-01-28 11:05:50 +0900

    add rule analyzer

---
+++ src/analyer/rule.js
@@ -0,0 +1,34 @@
+import Analyzer from 'analyzer';
+
+export default RuleAnalyzer;
+
+class RuleAnalyzer extends Analyzer {
+
+  constructor(rules = []) {
+    super();
+    this.rules = rules;
+  }
+
+  analyze() {
+    return {
+      cssDeclarations: this.getCSSDeclarations()
+    };
+  }
+
+  getCSSDeclarations() {
+
+    let declarations = [];
+
+    for (let rule of this.rules) {
+      if (Array.isArray(rule.declarations)) {
+        declarations.push({
+          selector: rule.selectors,
+          count: rule.declarations.length
+        });
+      }
+    }
+
+    declarations.sort((a, b) => b.count - a.count);
+  }
+}
+
